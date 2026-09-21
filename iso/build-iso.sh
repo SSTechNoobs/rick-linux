91f68a2bda8368bc2edecc32b0781681e7f7fb0b
Rick N <134166164+SSTechNoobs@users.noreply.github.com>
2026-09-21 15:19:35 -0500

    Pin Rick Install ISO to updated installer

--- iso/build-iso.sh
+++ iso/build-iso.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
 ISO_DIR="$ROOT/iso"
 
-PIN="2b9078cc1d7f85011c0ebe8431a7c9cf7d2fed11"
+PIN="2fc8e961e25b49cb523e45cfee2068564e769995"
 
 PROFILE="$HOME/.cache/rick-linux-iso-profile"
 WORK="$HOME/.cache/rick-linux-archiso-work"
